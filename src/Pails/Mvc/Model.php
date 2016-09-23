07a8ffea086bc1e98eaf8ced7949b69d424c6f8f
倪述荣 <xueron@xueron.com>
2016-09-23 15:38:55 +0800

    add method make()

--- src/Pails/Mvc/Model.php
+++ src/Pails/Mvc/Model.php
@@ -5,7 +5,42 @@
  */
 namespace Pails\Mvc;
 
+use Phalcon\Mvc\Model\Behavior\Timestampable;
+
 abstract class Model extends \Phalcon\Mvc\Model
 {
 
+    public function initialize()
+    {
+        // 自动更新时间戳字段
+        $this->addBehavior(new Timestampable([
+                'beforeCreate' => [
+                    'field' => ['updated_at'],
+                    'format' => 'Y-m-d H:i:s'
+                ],
+                'beforeUpdate' => [
+                    'field' => 'updated_at',
+                    'format' => 'Y-m-d H:i:s'
+                ]
+            ])
+        );
+    }
+
+    public static function make($data = [])
+    {
+        // filter empty values
+        $data = array_filter($data);
+
+        // remove auto data;
+        unset($data['id']);
+        unset($data['created_at']);
+        unset($data['updated_at']);
+
+        //
+        $model = new static();
+        if ($model->save($data)) {
+            return $model;
+        }
+        return false;
+    }
 }
