a13837564dccd421e2486dacd648e105941358bd
倪述荣 <xueron@xueron.com>
2016-12-24 21:14:38 +0800

    add some helper

--- src/Pails/Mvc/Controller.php
+++ src/Pails/Mvc/Controller.php
@@ -5,12 +5,19 @@
  */
 
 namespace Pails\Mvc;
+use Pails\Plugins\Fractal;
+use Pails\Plugins\Inflector;
+use Phalcon\Logger\AdapterInterface;
+
 /**
  * Class Controller
  * @package Pails\Mvc
  * @property \Pails\Plugins\ApiResponse $apiResponse
+ * @property \Phalcon\Security\Random $random
+ * @property Fractal $fractal
+ * @property Inflector $inflector
+ * @property AdapterInterface $logger
  */
-
 abstract class Controller extends \Phalcon\Mvc\Controller
 {
 
