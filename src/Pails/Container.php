5db7e51d0bfde4125dd930e428a6e7d6835e6524
倪述荣 <xueron@xueron.com>
2017-04-13 20:59:45 +0800

    增加文档。update version number to 3.1.3

--- src/Pails/Container.php
+++ src/Pails/Container.php
@@ -22,7 +22,7 @@ class Container extends Di\FactoryDefault implements ContainerInterface
     /**
      * Pails Version
      */
-    const VERSION = '3.1.1';
+    const VERSION = '3.1.3';
 
     /**
      * @var Loader
