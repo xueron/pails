4adfa3a8c7371c62529cc205110e0135bf8e7b53
倪述荣 <xueron@xueron.com>
2017-04-19 02:06:58 +0800

    allow return object as meta

--- src/Pails/Plugins/Paginator.php
+++ src/Pails/Plugins/Paginator.php
@@ -2,6 +2,7 @@
 /**
  * Paginator.php
  */
+
 namespace Pails\Plugins;
 
 use Phalcon\Paginator\Adapter\QueryBuilder;
@@ -27,6 +28,9 @@ class Paginator extends QueryBuilder
     }
 
     /**
+     * 结果数据集合，
+     *
+     * @return mixed
      */
     public function getData()
     {
@@ -43,22 +47,26 @@ class Paginator extends QueryBuilder
     /**
      * @return array|null
      */
-    public function getMeta()
+    public function getMeta($assoc = true)
     {
         if ($this->_paginator == null) {
             $this->_paginator = $this->getPaginate();
         }
         if ($this->_meta == null) {
-            $this->_meta = [
-                'first'       => $this->_paginator->first,
-                'before'      => $this->_paginator->before,
-                'current'     => $this->_paginator->current,
-                'last'        => $this->_paginator->last,
-                'next'        => $this->_paginator->next,
-                'total_pages' => $this->_paginator->total_pages,
-                'total_items' => $this->_paginator->total_items,
-                'limit'       => $this->_paginator->limit,
-            ];
+            if ($assoc) {
+                $this->_meta = [
+                    'first'       => $this->_paginator->first,
+                    'before'      => $this->_paginator->before,
+                    'current'     => $this->_paginator->current,
+                    'last'        => $this->_paginator->last,
+                    'next'        => $this->_paginator->next,
+                    'total_pages' => $this->_paginator->total_pages,
+                    'total_items' => $this->_paginator->total_items,
+                    'limit'       => $this->_paginator->limit,
+                ];
+            } else {
+                $this->_meta = $this->_paginator;
+            }
         }
 
         return $this->_meta;
