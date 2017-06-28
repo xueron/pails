6a68f4ad6547a82ff7566658b3d8c809e7f89481
倪述荣 <xueron@xueron.com>
2017-06-28 16:01:36 +0800

    code format

--- src/Pails/Listeners/Dispatcher.php
+++ src/Pails/Listeners/Dispatcher.php
@@ -16,41 +16,33 @@ class Dispatcher extends Injectable
 
     public function beforeDispatch($event, $dispatcher)
     {
-
     }
 
     public function beforeNotFoundAction($event, $dispatcher)
     {
-
     }
 
     public function beforeExecuteRoute($event, $dispatcher)
     {
-
     }
 
     public function afterInitialize($event, $dispatcher)
     {
-
     }
 
     public function afterBinding($event, $dispatcher)
     {
-
     }
 
     public function afterExecuteRoute($event, $dispatcher, $value)
     {
-
     }
 
     public function afterDispatch($event, $dispatcher)
     {
-
     }
 
     public function afterDispatchLoop($event, $dispatcher)
     {
-
     }
 }
