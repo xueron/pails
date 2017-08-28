908ad3a57e6af8e01f5a80daad0390da41635139
倪述荣 <xueron@xueron.com>
2017-08-28 13:19:43 +0800

    Fix Swagger Base path

--- src/Pails/Plugins/Swagger.php
+++ src/Pails/Plugins/Swagger.php
@@ -12,7 +12,7 @@ class Swagger extends Injectable
 {
     public function showApi()
     {
-        $swagger = \Swagger\scan($this->di->path('Http/Controllers'));
+        $swagger = \Swagger\scan($this->di->path('Http'));
         return $this->response->setContentType('application/json')
             ->setContent($swagger);
     }
